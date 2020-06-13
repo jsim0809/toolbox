fc355c964c6fcef545be536a6a0ba929aff55813
Jeremy Sim <jsim0809@gmail.com>
2020-06-13 16:43:52 -0700

    Add module.exports functionality

--- fisherYates.js
+++ fisherYates.js
@@ -1,10 +1,10 @@
 /** 
 * Implements the inside-out Fisher-Yates algorithm, a technique for efficiently creating a random ordering of array elements.
-* Example call: fisherYates(9)
+* Example call: generate(9)
 * Example output: [ 1, 8, 3, 7, 4, 2, 0, 6, 5 ]
 */
 
-const fisherYates = (n) => {
+module.exports.generate = (n) => {
   const result = []
   for (let i = 0; i < n; i += 1) {
     const swapIndex = Math.floor(Math.random() * (i + 1));
